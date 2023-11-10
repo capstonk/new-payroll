ca4c8407b7bd35a1a07985642b496c4fb3660ba8
rczhoce <144341667+rczhoce@users.noreply.github.com>
2023-11-10 13:19:32 +0800

    Delete online-payroll directory

--- online-payroll/script/show-password.js
+++
@@ -1,9 +0,0 @@
-const showPassword = document.querySelector("#show-password");
-const passwordField = document.querySelector("#password");
-
-showPassword.addEventListener("click", function () {
-  this.classList.toggle("fa-eye");
-  this.classList.toggle("fa-eye-slash", !this.classList.contains("fa-eye"));
-  const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
-  passwordField.setAttribute("type", type);
-});
